27fed5ab069ed450f46b934e3bf255331ddadae8
guangyue.cao@perkinswill.com <gc2663@columbia.edu>
2023-03-27 00:06:43 -0700

    established streamlit framework with base grid geom

--- app/utils.py
+++ app/utils.py
@@ -5,9 +5,9 @@ from shapely import geometry
 # import folium
 import pandas as pd
  
-def create_grid(lat, lng, radius, size):
+def create_grid(lat=37.78, lng=-122.39, radius=800, size=200):
     # point from lat, lng
-    df = pd.DataFrame({'lat': [37.49745808488778], 'lng': [-122.24989833795438]})
+    df = pd.DataFrame({'lat': [lat], 'lng': [lng]})
     point = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['lng'], df['lat'],crs="EPSG:4326"))
 
     # boundary file
@@ -38,5 +38,6 @@ def create_grid(lat, lng, radius, size):
 
     grid_gdf = fishnet.sjoin(boundary, how = 'inner')
     grid_gdf = grid_gdf.drop(['index_right'],axis = 1)
+    grid_gdf = grid_gdf.to_crs('EPSG:4326')
     
     return grid_gdf
