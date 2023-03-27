6270bb41dd79d6a1a21c9abf3094fd79656a4372
guangyue.cao@perkinswill.com <gc2663@columbia.edu>
2023-03-26 18:19:11 -0700

    Create a new branch

--- app/utils.py
+++ app/utils.py
@@ -1,17 +1,24 @@
-from shapely import geometry
+import os
+os.environ['USE_PYGEOS'] = '0'
 import geopandas as gpd
+from shapely import geometry
+# import folium
+import pandas as pd
  
-def create_grid(lat, lng, radius, grid_size):
+def create_grid(lat, lng, radius, size):
     # point from lat, lng
-    points_gdf = gpd.points_from_xy(lat, lng)
+    df = pd.DataFrame({'lat': [37.49745808488778], 'lng': [-122.24989833795438]})
+    point = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['lng'], df['lat'],crs="EPSG:4326"))
 
     # boundary file
-    boundary = points_gdf.buffer(radius)
+    # distance in meters
+    boundary = point.to_crs('EPSG:3857').buffer(distance = radius)
+    grid_size = size
 
     # Get the extent of the shapefile
     # Get minX, minY, maxX, maxY
+    boundary = boundary.to_crs("EPSG:3857")
     minX, minY, maxX, maxY = boundary.total_bounds
-
     # Create a fishnet
     x, y = (minX, minY)
     geom_array = []
@@ -25,5 +32,11 @@ def create_grid(lat, lng, radius, grid_size):
         y += grid_size
 
     fishnet = gpd.GeoDataFrame(geom_array, columns=['geometry']).set_crs(boundary.crs)
+    boundary = gpd.GeoDataFrame(boundary)
+    boundary = boundary.set_geometry(boundary[0])
+    boundary = boundary.drop([0],axis = 1)
+
     grid_gdf = fishnet.sjoin(boundary, how = 'inner')
+    grid_gdf = grid_gdf.drop(['index_right'],axis = 1)
+    
     return grid_gdf
