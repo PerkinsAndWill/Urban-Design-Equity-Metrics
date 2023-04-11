daf2fcd3398bf1ae2dd914a4327a3603287e0fa7
guangyue.cao@perkinswill.com <gc2663@columbia.edu>
2023-04-11 01:29:44 -0700

    get_shapefile() WIP

--- app/myapp.py
+++ app/myapp.py
@@ -3,6 +3,7 @@ import folium
 import geopandas as gpd
 import pandas as pd
 from utils import create_grid, get_fips_code
+from get_census import get_county_census
 from streamlit_folium import folium_static
 import json
 
@@ -43,12 +44,18 @@ grid_df = create_grid(lat, lng, radius=800, size=200)
 
 
 # GET CENSUS DATA
-
+# REMINDER: THIS IS WITHOUT GEOMETRY COLUMN
+df_county_census = get_county_census(lat,lng, options)
 
 # GET RETAILS AND TRANSIT STOPS
 
 
 ########### FEATURE ENGINEERING #################
+
+
+
+
+########### ENRICH GRID #################
 
 
 ########### DISPLAY MAP ##################
